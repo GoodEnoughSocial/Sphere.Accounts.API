d2ae370cac0aea354779d88dede848f6057f6153
Chad Jessup <chad.jessup@gmail.com>
2022-07-11 01:00:33 -0700

    couple basic test endpoints to get a feel for grains

---
+++ Sphere.Accounts.API/Controllers/AccountsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orleans;
+
+namespace Sphere.Accounts.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountsController : Controller
+    {
+        private readonly ILogger<AccountsController> _logger;
+        private readonly IClusterClient _client;
+
+        public AccountsController(
+            ILogger<AccountsController> logger,
+            IClusterClient clusterClient)
+        {
+            _logger = logger;
+            _client = clusterClient;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Sphere.Accounts.API/Controllers/WeatherForecastController.cs
+++ Sphere.Accounts.API/Controllers/WeatherForecastController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Orleans;
+using Sphere.Interfaces;
+using Sphere.Shared.Models;
 
 namespace Sphere.Accounts.API.Controllers;
 
@@ -12,15 +15,34 @@ public class WeatherForecastController : ControllerBase
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IClusterClient _client;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController(
+        ILogger<WeatherForecastController> logger,
+        IClusterClient client)
     {
         _logger = logger;
+        _client = client;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    public async Task<IEnumerable<WeatherForecast>> Get()
     {
+        var accountGrain = _client.GetGrain<IAccount>("test");
+        var state = await accountGrain.GetAccountState();
+        // await accountGrain.RegisterAccount(new AccountState
+        // {
+        //     AccountDisplayName = "Test Name",
+        //     AccountId = 2,
+        //     BirthDate = new DateOnly(1979, 08, 11),
+        //     CreatedAt = DateTime.UtcNow,
+        //     CreatedWith = CreatedWith.Web,
+        //     Email = "test.user@gmail.com",
+        //     TimeZone = TimeZoneInfo.Local.ToSerializedString(),
+        //     UserName = "@TestUser",
+        //     PhoneNumber = "+15555551212",
+        // });
+
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateTime.Now.AddDays(index),
